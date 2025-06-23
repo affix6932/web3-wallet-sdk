66a57a85d35a42b0c0756ffbaaf5e230aecc7658
aboxd <aboxd@outlook.com>
2025-06-23 22:24:32 +0800

    feat: add utils.js

---
+++ util.js
@@ -0,0 +1,460 @@
+export const chainNamesObj = {
+  // Ethereum
+  '0x1': 'Ethereum Mainnet',
+  '0x3': 'Ropsten Testnet',
+  '0x4': 'Rinkeby Testnet',
+  '0x5': 'Goerli Testnet',
+  '0x2a': 'Kovan Testnet',
+  
+  // Binance Smart Chain
+  '0x38': 'Binance Smart Chain',
+  '0x61': 'BSC Testnet',
+  
+  // Polygon
+  '0x89': 'Polygon Mainnet',
+  '0x13881': 'Mumbai Testnet',
+  
+  // Avalanche
+  '0xa86a': 'Avalanche C-Chain',
+  '0xa869': 'Avalanche Fuji Testnet',
+  
+  // Fantom
+  '0xfa': 'Fantom Opera',
+  '0xfa2': 'Fantom Testnet',
+  
+  // Arbitrum
+  '0xa4b1': 'Arbitrum One',
+  '0x66eed': 'Arbitrum Goerli',
+  
+  // Optimism
+  '0xa': 'Optimism',
+  '0x1a4': 'Optimism Goerli',
+  
+  // HECO
+  '0x80': 'Huobi ECO Chain',
+  '0x100': 'HECO Testnet',
+  
+  // OKEx Chain
+  '0x42': 'OKExChain',
+  '0x41': 'OKExChain Testnet',
+  
+  // xDai
+  '0x64': 'Gnosis Chain (xDai)',
+  
+  // Harmony
+  '0x63564c40': 'Harmony Mainnet',
+  '0x6357d2e0': 'Harmony Testnet',
+  
+  // Celo
+  '0xa4ec': 'Celo Mainnet',
+  '0xaef3': 'Celo Alfajores Testnet',
+  
+  // Aurora
+  '0x4e454152': 'Aurora Mainnet',
+  '0x4e454153': 'Aurora Testnet',
+  
+  // Cronos
+  '0x19': 'Cronos Mainnet',
+  '0x152': 'Cronos Testnet',
+  
+  // Boba Network
+  '0x120': 'Boba Network',
+  '0x12047': 'Boba Rinkeby Testnet',
+  
+  // Moonbeam
+  '0x504': 'Moonbeam',
+  '0x507': 'Moonbase Alpha',
+  
+  // Astar
+  '0x250': 'Astar',
+  '0x51': 'Shibuya Testnet',
+  
+  // KCC
+  '0x141': 'KCC Mainnet',
+  '0x142': 'KCC Testnet',
+  
+  // Velas
+  '0x6a': 'Velas',
+  '0x6f': 'Velas Testnet',
+  
+  // IoTeX
+  '0x2cf5': 'IoTeX Mainnet',
+  '0x2d37': 'IoTeX Testnet',
+  
+  // Fuse
+  '0x7a': 'Fuse Mainnet',
+  '0x7b': 'Fuse Sparknet',
+  
+  // Evmos
+  '0x2328': 'Evmos',
+  '0x5ae': 'Evmos Testnet',
+  
+  // Godwoken
+  '0x116e9': 'Godwoken Mainnet',
+  '0x1d5f': 'Godwoken Testnet',
+  
+  // Metis
+  '0x440': 'Metis Andromeda',
+  '0x440': 'Metis Stardust Testnet',
+  
+  // SmartBCH
+  '0x2710': 'SmartBCH',
+  '0x2711': 'SmartBCH Testnet',
+  
+  // Telos
+  '0x28': 'Telos EVM Mainnet',
+  '0x29': 'Telos EVM Testnet',
+  
+  // Oasis
+  '0xa516': 'Oasis Emerald',
+  '0xa515': 'Oasis Testnet',
+  
+  // ThunderCore
+  '0x6c': 'ThunderCore Mainnet',
+  '0x18': 'ThunderCore Testnet',
+  
+  // RSK
+  '0x1e': 'RSK Mainnet',
+  '0x1f': 'RSK Testnet',
+  
+  // ETC
+  '0x3d': 'Ethereum Classic',
+  '0x3f': 'Mordor Testnet',
+  
+  // POA
+  '0x63': 'POA Network',
+  '0x4a': 'POA Sokol',
+  
+  // TomoChain
+  '0x58': 'TomoChain',
+  '0x59': 'TomoChain Testnet',
+  
+  // Wanchain
+  '0x8': 'Wanchain',
+  '0x3e9': 'Wanchain Testnet',
+  
+  // KardiaChain
+  '0x1ae6': 'KardiaChain',
+  '0x1ae7': 'KardiaChain Testnet',
+  
+  // Elastos
+  '0x14': 'Elastos',
+  '0x15': 'Elastos Testnet',
+  
+  // GoChain
+  '0x3c': 'GoChain',
+  '0x3d': 'GoChain Testnet',
+  
+  // Callisto
+  '0x80': 'Callisto',
+  '0x81': 'Callisto Testnet',
+  
+  // EOS EVM
+  '0x2a15c308d': 'EOS EVM',
+  '0x2a15c3083': 'EOS EVM Testnet',
+  
+  // Meter
+  '0x52': 'Meter',
+  '0x53': 'Meter Testnet',
+  
+  // Nahmii
+  '0x73': 'Nahmii',
+  '0x4e': 'Nahmii Testnet',
+  
+  // Energy Web
+  '0xf6': 'Energy Web',
+  '0xf7': 'Energy Web Testnet',
+  
+  // Fuse Sparknet
+  '0x7a': 'Fuse Sparknet',
+  '0x7b': 'Fuse Sparknet Testnet',
+  
+  // KCC
+  '0x141': 'KCC',
+  '0x142': 'KCC Testnet',
+  
+  // Shiden
+  '0x150': 'Shiden',
+  '0x151': 'Shiden Testnet',
+  
+  // SX Network
+  '0x2b': 'SX Network',
+  '0x2c': 'SX Network Testnet',
+  
+  // Ubiq
+  '0x8': 'Ubiq',
+  '0x9': 'Ubiq Testnet',
+  
+  // VELAS
+  '0x6a': 'Velas',
+  '0x6f': 'Velas Testnet',
+  
+  // ZYX
+  '0x1b': 'ZYX',
+  '0x1c': 'ZYX Testnet',
+  
+  // Zilliqa
+  '0x8000008b': 'Zilliqa',
+  '0x8000008c': 'Zilliqa Testnet',
+  
+  // ZKSpace
+  '0x1e': 'ZKSpace',
+  '0x1f': 'ZKSpace Testnet',
+  
+  // ZYX
+  '0x1b': 'ZYX',
+  '0x1c': 'ZYX Testnet',
+  
+  // 添加更多链...
+}; 
+
+export const testnetIdList = [
+  // Ethereum
+  '0x3',  // Ropsten
+  '0x4',  // Rinkeby
+  '0x5',  // Goerli
+  '0x2a', // Kovan
+  '0xaa36a7', // Sepolia
+  
+  // Binance Smart Chain
+  '0x61', // BSC Testnet
+  
+  // Polygon
+  '0x13881', // Mumbai
+  
+  // Avalanche
+  '0xa869', // Fuji Testnet
+  
+  // Fantom
+  '0xfa2', // Fantom Testnet
+  
+  // Arbitrum
+  '0x66eed', // Arbitrum Goerli
+  
+  // Optimism
+  '0x1a4', // Optimism Goerli
+  
+  // HECO
+  '0x100', // HECO Testnet
+  
+  // OKEx Chain
+  '0x41', // OKExChain Testnet
+  
+  // Harmony
+  '0x6357d2e0', // Harmony Testnet
+  
+  // Celo
+  '0xaef3', // Celo Alfajores Testnet
+  
+  // Aurora
+  '0x4e454153', // Aurora Testnet
+  
+  // Cronos
+  '0x152', // Cronos Testnet
+  
+  // Boba Network
+  '0x12047', // Boba Rinkeby Testnet
+  
+  // Moonbeam
+  '0x507', // Moonbase Alpha
+  
+  // Astar
+  '0x51', // Shibuya Testnet
+  
+  // KCC
+  '0x142', // KCC Testnet
+  
+  // Velas
+  '0x6f', // Velas Testnet
+  
+  // IoTeX
+  '0x2d37', // IoTeX Testnet
+  
+  // Evmos
+  '0x5ae', // Evmos Testnet
+  
+  // Godwoken
+  '0x1d5f', // Godwoken Testnet
+  
+  // SmartBCH
+  '0x2711', // SmartBCH Testnet
+  
+  // Telos
+  '0x29', // Telos EVM Testnet
+  
+  // Oasis
+  '0xa515', // Oasis Testnet
+  
+  // ThunderCore
+  '0x18', // ThunderCore Testnet
+  
+  // RSK
+  '0x1f', // RSK Testnet
+  
+  // ETC
+  '0x3f', // Mordor Testnet
+  
+  // TomoChain
+  '0x59', // TomoChain Testnet
+  
+  // Wanchain
+  '0x3e9', // Wanchain Testnet
+  
+  // KardiaChain
+  '0x1ae7', // KardiaChain Testnet
+  
+  // Elastos
+  '0x15', // Elastos Testnet
+  
+  // GoChain
+  '0x3d', // GoChain Testnet
+  
+  // Callisto
+  '0x81', // Callisto Testnet
+  
+  // EOS EVM
+  '0x2a15c3083', // EOS EVM Testnet
+  
+  // Meter
+  '0x53', // Meter Testnet
+  
+  // Nahmii
+  '0x4e', // Nahmii Testnet
+  
+  // Energy Web
+  '0xf7', // Energy Web Testnet
+  
+  // Fuse Sparknet
+  '0x7b', // Fuse Sparknet Testnet
+  
+  // Shiden
+  '0x151', // Shiden Testnet
+  
+  // SX Network
+  '0x2c', // SX Network Testnet
+  
+  // Ubiq
+  '0x9', // Ubiq Testnet
+  
+  // Zilliqa
+  '0x8000008c', // Zilliqa Testnet
+  
+  // ZKSpace
+  '0x1f', // ZKSpace Testnet
+  
+  // ZYX
+  '0x1c' // ZYX Testnet
+];
+
+export const CHAINS = {
+  BTC: {
+    chainId: '0x0', // 0 in decimal
+    name: 'Bitcoin',
+    nativeCurrency: 'BTC',
+    tokens: ['BTC']
+  },
+  ETH: {
+    chainId: '0x1', // 1 in decimal
+    name: 'Ethereum',
+    nativeCurrency: 'ETH',
+    tokens: ['ETH', 'USDT', 'USDC'],
+    usdt_address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+    usdc_address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+  },
+  // testnet used for sepolia testing
+  SEPOLIA: {
+    chainId: '0xaa36a7', // 728126428 in decimal
+    name: 'Sepolia',
+    nativeCurrency: 'ETH',
+    tokens: ['ETH', 'PYUSDT'],
+    pyusdt_address: '0xCaC524BcA292aaade2DF8A05cC58F0a65B1B3bB9',
+  },
+  TRON: {
+    chainId: '0x2b6653dc', // 728126428 in decimal
+    name: 'TRON',
+    nativeCurrency: 'TRX',
+    tokens: ['TRX', 'USDT'],
+    usdt_address: 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t',
+  },
+  SOL: {
+    chainId: '0x5a', // 90 in decimal (unofficial, as Solana doesn't use EVM chainId)
+    name: 'Solana',
+    nativeCurrency: 'SOL',
+    tokens: ['SOL', 'USDT', 'USDC'],
+    usdt_address: 'Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB',
+    usdc_address: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
+  },
+  ARBITRUM: {
+    chainId: '0xa4b1', // 42161 in decimal
+    name: 'Arbitrum One',
+    nativeCurrency: 'ETH',
+    tokens: ['ETH', 'USDT', 'ARB'],
+    usdt_address: '0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9',
+    arb_address: '0x912ce59144191c1204e64559fe8253a0e49e6548',
+  },
+  OPTIMISM: {
+    chainId: '0xa', // 10 in decimal
+    name: 'Optimism',
+    nativeCurrency: 'ETH',
+    tokens: ['ETH', 'USDT'],
+    usdt_address: '0x94b008aa00579c1307b0ef2c499ad98a8ce58e58'
+  },
+  AVAXC: {
+    chainId: '0xa86a', // 43114 in decimal
+    name: 'Avalanche C-Chain',
+    nativeCurrency: 'AVAX',
+    tokens: ['AVAX', 'USDT'],
+    usdt_address: '0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7',
+  },
+  BSC: {
+    chainId: '0x38', // 56 in decimal
+    name: 'BNB Smart Chain',
+    nativeCurrency: 'BNB',
+    tokens: ['BNB', 'USDT'],
+    usdt_address: '0x55d398326f99059ff775485246999027b3197955',
+  },
+  POLYGON: {
+    chainId: '0x89', // 137 in decimal
+    name: 'Polygon',
+    nativeCurrency: 'POL',
+    tokens: ['POL', 'USDT'],
+    usdt_address: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f',
+  },
+  XRP: {
+    chainId: '0x3d', // 61 in decimal (XRP Ledger doesn't use EVM chainId)
+    name: 'XRP Ledger',
+    nativeCurrency: 'XRP',
+    tokens: ['XRP']
+  },
+  TON: {
+    chainId: '0x28c', // 652 in decimal (unofficial, as TON doesn't use EVM chainId)
+    name: 'The Open Network',
+    nativeCurrency: 'TON',
+    tokens: ['TON', 'USDT'],
+    usdt_address: 'EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs',
+  },
+  BCH: {
+    chainId: '0x91a', // 2330 in decimal (unofficial, as BCH doesn't use EVM chainId)
+    name: 'Bitcoin Cash',
+    nativeCurrency: 'BCH',
+    tokens: ['BCH']
+  },
+  DOGE: {
+    chainId: 'x3c', // 60 in decimal (unofficial, as Dogecoin doesn't use EVM chainId)
+    name: 'Dogecoin',
+    nativeCurrency: 'DOGE',
+    tokens: ['DOGE']
+  },
+  LTC: {
+    chainId: '0x7c', // 124 in decimal (unofficial, as Litecoin doesn't use EVM chainId)
+    name: 'Litecoin',
+    nativeCurrency: 'LTC',
+    tokens: ['LTC']
+  },
+  ADA: {
+    chainId: '0x3e8', // 1000 in decimal (unofficial, as Cardano doesn't use EVM chainId)
+    name: 'Cardano',
+    nativeCurrency: 'ADA',
+    tokens: ['ADA']
+  }
+};
